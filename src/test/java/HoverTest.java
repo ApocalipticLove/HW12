78d247c86dac2168f43d1204a18cfd8177253fd1
Dmitrii <Apocaliptic.love@gmail.com>
2022-12-19 17:09:05 +0400

    Initial commit

---
+++ src/test/java/HoverTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HoverTest {
+    @Test
+    public void hoverGitHubTest(){
+        open("https://github.com/");
+        $$(".HeaderMenu-link").findBy(text("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        $(".col-9-max").shouldHave(Condition.text("Build like the best"));
+    }
+    @Test
+    public void dragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+
+    }
+}
